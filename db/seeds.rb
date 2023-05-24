60cfb69e17f6a1a6add23a0b3aaf235e31f2a8f4
damien leduc <damien.led69@gmail.com>
2023-05-24 15:34:46 +0200

    ...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+restaurant1 = Restaurant.create!([ name: "Le chonchon",  category: "french", address: "5 rue de la pinte" ])
+restaurant2 = Restaurant.create([ name: "Le torchon",  category: "belgian",  address: "10 rue de ici" ])
+restaurant3 = Restaurant.create([ name: "Le ronchon",  category: "japanese",  address: "une addresse ici" ])
+restaurant4 = Restaurant.create([ name: "Le bourbon",  category: "italian",  address: "une autre addresse ici" ])
+restaurant5 = Restaurant.create([ name: "Le chameau",  category: "chinese",  address: "au meme endroit" ])
